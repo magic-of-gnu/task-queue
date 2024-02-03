924fda94b2748e8d2ce95ac097e194ff6dc5f2e4
Daniyar Bakir <daniyar.bakir@gmail.com>
2024-02-03 13:36:27 +0000

    added len method

--- project/subqueue.py
+++ project/subqueue.py
@@ -6,6 +6,9 @@ class SubQueue:
     name: str
     tasks: dict  # ordered by default; set is not ordered
 
+    def __len__(self):
+        return len(self.tasks)
+
     def popleft(self):
         for k in self.tasks.keys():
             v = self.tasks.pop(k)
